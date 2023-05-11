3f67fff48f1084d36bd81bfde26e8113aae27ac9
andyjjrt <andyjjrt@gmail.com>
2023-05-12 03:31:08 +0800

    refactor: fetchRate

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-import os, json
+from bs4 import BeautifulSoup
+import os, json, logging, tqdm, requests
 
 from fetchClass import fetchClass
 from fetchTeacherId import fetchTeacherId
 from fetchRate import fetchRate
 from translateRate import translateRate
+from fetchDescription import fetchDescription
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def useOldClasses():
-  with open(os.path.join(dir_path, "_data", "classes.json"), "r") as f:
-    return json.loads(f.read())
+if __name__ == "__main__":
+  
+  logging.basicConfig(filename='example.log', format='%(asctime)s [%(levelname)s] %(message)s', encoding='utf-8')
+  
+  with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "_data", "teachers.json"), "r") as f:
+    newTeacherList = json.loads(f.read())
+  with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "old_data", "1111_teachers.json"), "r") as f:
+    oldTeacherList = json.loads(f.read())
+  
+  teacherList = {**newTeacherList,**oldTeacherList}
+  
+  teachers = tqdm.tqdm(teacherList, total=len(teacherList), leave=False)
+  for teacher in teachers:
+    try:
+      teacherId = teacherList[teacher]
+      teachers.set_postfix_str("processing: {} {}".format(teacherId, teacher))
+      location = "http://newdoc.nccu.edu.tw/teaschm/1082/statistic.jsp-tnum={}.htm".format(teacherId)
+      res = requests.get(location)
+      res.raise_for_status()
+      soup = BeautifulSoup(res.content.decode("big5").encode("utf-8"), "html.parser")
+      courses = soup.find_all('table')[2].find_all('tr')
+      tqdmCourses = tqdm.tqdm(courses, total=len(courses), leave=False)
+      
+      for row in tqdmCourses:
+        cols = row.find_all('td')
+        courseId = "{}{}{}".format(cols[0].text, cols[1].text, cols[2].text)
+        tqdmCourses.set_postfix_str("processing: {}".format(courseId))
+        if cols[-1].find("a"):
+          detail = fetchDescription(courseId)
+          rates = fetchRate("http://newdoc.nccu.edu.tw/teaschm/1082/" + cols[-1].find("a")["href"])
+          logging.debug(detail)
+          logging.debug(rates)
+    except Exception as e:
+      logging.error(e)
+      continue
+  
 
-def useOldTeachers():
-  with open(os.path.join(dir_path, "_data", "teachers.json"), "r") as f:
-    return json.loads(f.read())
-
-if not os.path.exists(os.path.join(dir_path, "_data")):
-  os.makedirs(os.path.join(dir_path, "_data"))
-
-classes = fetchClass()
-TeacherId = fetchTeacherId(useOldClasses())
-fetchRate(TeacherId)
-
-translateRate()
+# translateRate()
