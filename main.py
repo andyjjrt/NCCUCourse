c5043b499121a5f8c95bbbf829d02e157e70b5db
andyjjrt <andyjjrt@gmail.com>
2023-05-12 17:50:38 +0800

    feat: Add prompt and cli options

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import sys
 from bs4 import BeautifulSoup
 from time import sleep
-import os, json, logging, tqdm, requests, datetime
+import os, json, logging, tqdm, requests, datetime, getopt
 
 from User import User
 from fetchDescription import fetchDescription
 from fetchRate import fetchRate
 from translateRate import translateRate
 
+options = "h"
+longOptions = ["help", "skip_class", "skip_teacher", "skip_rate"]
+argumentList = sys.argv[1:]
+
+programOptions = {
+  "skip_class": False,
+  "skip_teacher": False,
+  "skip_rate": False
+}
 
 dirPath = os.path.dirname(os.path.realpath(__file__))
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
+  try:
+    arguments, values = getopt.getopt(argumentList, options, longOptions)
+    for currentArgument, currentValue in arguments:
+      if currentArgument in ("-h", "--help"):
+        print ("""
+  Usage: python main.py [OPTIONS]...
+
+  Options:
+    --skip_class          Skip fetch class operation.
+    --skip_teacher        Skip fetch teacher operation.
+    --skip_rate           Skip fetch rate operation.
+    --help, -h            Display this help and exit.            
+""")
+        sys.exit()
+      elif currentArgument in ("--skip_class"):
+        programOptions["skip_class"] = True
+      elif currentArgument in ("--skip_teacher"):
+        programOptions["skip_teacher"] = True
+      elif currentArgument in ("--skip_rate"):
+        programOptions["skip_rate"] = True
+  except getopt.error as err:
+    print (str(err))
+    sys.exit()
+    
   # Setup logger
-  logging.basicConfig(filename='example.log', format='%(asctime)s [%(levelname)s] %(message)s', encoding='utf-8')
+  logging.basicConfig(filename='log.log', format='%(asctime)s [%(levelname)s] %(message)s', encoding='utf-8')
   
   if(os.path.exists(os.path.join(dirPath, "_data"))):
     os.makedirs(os.path.join(dirPath, "_data"), exist_ok=True)
@@ -22,159 +55,178 @@ if __name__ == "__main__":
   # \ 1. Fetch Classes           \
   # ==============================
   
-  # fetch all deps first, make a single search less than 500
-  units = requests.get("https://qrysub.nccu.edu.tw/assets/api/unit.json")
-  units.raise_for_status()
-  categories = list()
-  for dp1 in [x for x in units.json() if x["utCodL1"] != "0"]:
-    for dp2 in [x for x in dp1["utL2"] if x["utCodL2"] != "0"]:
-      for dp3 in [x for x in dp2["utL3"] if x["utCodL3"] != "0"]:
-        categories.append({"dp1": dp1["utCodL1"], "dp2": dp2["utCodL2"], "dp3": dp3["utCodL3"]})
-  
-  # run through all deps, get their classId
-  coursesList = list()
-  tqdmCategories = tqdm.tqdm(categories, leave=False)
-  for category in tqdmCategories:
-    try:
-      tqdmCategories.set_postfix_str("processing: {}".format(category))
-      sleep(0.1)
-      res = requests.get("https://es.nccu.edu.tw/course/zh-TW/:sem=1112 :dp1={} :dp2={} :dp3={}".format(category["dp1"], category["dp2"], category["dp3"]))
-      res.raise_for_status()
-      courses = res.json()
-      if len(courses) >= 500:
-        raise Exception("{} too large".format(category))
-      logging.debug([x["subNum"] for x in courses])
-      coursesList += [x["subNum"] for x in courses]
-    except Exception as e:
-      logging.error(e)
-      raise(e)
-  logging.debug(coursesList)
-  
-  # Write courseList back to file
-  with open(os.path.join(dirPath, "_data", "classes.json"), "w+") as f:
-    f.write(json.dumps(coursesList))
-    f.close()
-  
-  print("Fetch Class done at {}".format(datetime.datetime.now()))
+  if not programOptions["skip_class"]:
+    # fetch all deps first, make a single search less than 500
+    units = requests.get("https://qrysub.nccu.edu.tw/assets/api/unit.json")
+    units.raise_for_status()
+    categories = list()
+    for dp1 in [x for x in units.json() if x["utCodL1"] != "0"]:
+      for dp2 in [x for x in dp1["utL2"] if x["utCodL2"] != "0"]:
+        for dp3 in [x for x in dp2["utL3"] if x["utCodL3"] != "0"]:
+          categories.append({"dp1": dp1["utCodL1"], "dp2": dp2["utCodL2"], "dp3": dp3["utCodL3"]})
+    
+    # run through all deps, get their classId
+    coursesList = list()
+    tqdmCategories = tqdm.tqdm(categories, leave=False)
+    for category in tqdmCategories:
+      try:
+        tqdmCategories.set_postfix_str("processing: {}".format(category))
+        sleep(0.1)
+        res = requests.get("https://es.nccu.edu.tw/course/zh-TW/:sem=1112 :dp1={} :dp2={} :dp3={}".format(category["dp1"], category["dp2"], category["dp3"]))
+        res.raise_for_status()
+        courses = res.json()
+        if len(courses) >= 500:
+          raise Exception("{} too large".format(category))
+        logging.debug([x["subNum"] for x in courses])
+        coursesList += [x["subNum"] for x in courses]
+      except Exception as e:
+        logging.error(e)
+        raise(e)
+    logging.debug(coursesList)
+    
+    # Write courseList back to file
+    with open(os.path.join(dirPath, "_data", "classes.json"), "w+") as f:
+      f.write(json.dumps(coursesList))
+      f.close()
+    
+    print("Fetch Class done at {}".format(datetime.datetime.now()))
+  else:
+    print("Skipping Fetch Class")
     
   # ==============================
   # \ 2. Fetch TeacherId         \
   # ==============================
   
-  # Read course list
-  with open(os.path.join(dirPath, "_data", "classes.json"), "r") as f:
-    coursesList = json.loads(f.read())
+  if not programOptions["skip_teacher"]:
+    # Read course list
+    with open(os.path.join(dirPath, "_data", "classes.json"), "r") as f:
+      coursesList = json.loads(f.read())
+    
+    user = User()
   
-  user = User()
- 
-  # Delete exist track courses before adding
-  courses = user.getTrack()
-  if len(courses) > 0:
+    # Delete exist track courses before adding
+    courses = user.getTrack()
+    if len(courses) > 0:
+      tqdmCourses =  tqdm.tqdm(courses, leave=False)
+      for course in tqdmCourses:
+        try:
+          sleep(0.2)
+          courseId = str(course["subNum"])
+          tqdmCourses.set_postfix_str("Pre-deleting {}".format(courseId))
+          user.deleteTrack(courseId)
+        except Exception as e:
+          logging.error(e)
+          continue
+    
+    # Add courses to track list
+    tqdmCourses = tqdm.tqdm(coursesList, leave=False)
+    for courseId in tqdmCourses:
+      try:
+        sleep(0.2)
+        tqdmCourses.set_postfix_str("Adding {}".format(courseId))
+        user.addTrack(courseId)
+      except Exception as e:
+        logging.error(e)
+        continue
+    
+    # get track list and parse out teacher id
+    courses = user.getTrack()
+    teacherIdDict = dict()
+    tqdmCourses = tqdm.tqdm(courses, leave=False)
+    for course in tqdmCourses:
+      try:
+        teacherStatUrl = str(course["teaStatUrl"])
+        teacherName = str(course["teaNam"])
+        tqdmCourses.set_postfix_str("Processing {}".format(teacherName))
+        if teacherStatUrl.startswith("https://newdoc.nccu.edu.tw/teaschm/1112/statisticAll.jsp"):
+          teacherId = teacherStatUrl.split("https://newdoc.nccu.edu.tw/teaschm/1112/statisticAll.jsp-tnum=")[1].split(".htm")[0]
+          teacherIdDict[teacherName] = teacherId
+        elif teacherStatUrl.startswith("https://newdoc.nccu.edu.tw/teaschm/1112/set20.jsp"):
+          # use ip to avoid name resolve error, and add time out
+          res = requests.get(teacherStatUrl.replace("newdoc.nccu.edu.tw", "140.119.229.20").replace("https://", "http://"), timeout=10)
+          res.raise_for_status()
+          sleep(0.2)
+          soup = BeautifulSoup(res.content.decode("big5").encode("utf-8"), "html.parser")
+          rows = soup.find_all("tr")
+          for row in [x.find_all("td") for x in soup.find_all("tr") if x.find_all("td")[1].find("a")]:
+            teacherName = str(row[0].text)
+            teacherId = row[-1].find("a")["href"].split("statisticAll.jsp-tnum=")[1].split(".htm")[0]
+            teacherIdDict[teacherName] = teacherId
+      except Exception as e:
+        logging.error(e)
+        continue
+        
+    # Write teacherIdDict back to file
+    with open(os.path.join(dirPath, "_data", "teachers.json"), "w+") as f:
+      f.write(json.dumps(teacherIdDict))
+      f.close()
+    
+    # Delete courses from track list
     tqdmCourses =  tqdm.tqdm(courses, leave=False)
     for course in tqdmCourses:
       try:
         sleep(0.2)
         courseId = str(course["subNum"])
-        tqdmCourses.set_postfix_str("Pre-deleting {}".format(courseId))
+        tqdmCourses.set_postfix_str("Deleting {}".format(courseId))
         user.deleteTrack(courseId)
       except Exception as e:
         logging.error(e)
         continue
-  
-  # Add courses to track list
-  tqdmCourses = tqdm.tqdm(coursesList, leave=False)
-  for courseId in tqdmCourses:
-    try:
-      sleep(0.2)
-      tqdmCourses.set_postfix_str("Adding {}".format(courseId))
-      user.addTrack(courseId)
-    except Exception as e:
-      logging.error(e)
-      continue
-  
-  # get track list and parse out teacher id
-  courses = user.getTrack()
-  teacherIdDict = dict()
-  tqdmCourses = tqdm.tqdm(courses, leave=False)
-  for course in tqdmCourses:
-    try:
-      teacherStatUrl = str(course["teaStatUrl"])
-      teacherName = str(course["teaNam"])
-      tqdmCourses.set_postfix_str("Processing {}".format(teacherName))
-      if teacherStatUrl.startswith("https://newdoc.nccu.edu.tw/teaschm/1112/statisticAll.jsp"):
-        teacherId = teacherStatUrl.split("https://newdoc.nccu.edu.tw/teaschm/1112/statisticAll.jsp-tnum=")[1].split(".htm")[0]
-        teacherIdDict[teacherName] = teacherId
-      elif teacherStatUrl.startswith("https://newdoc.nccu.edu.tw/teaschm/1112/set20.jsp"):
-        # use ip to avoid name resolve error, and add time out
-        res = requests.get(teacherStatUrl.replace("newdoc.nccu.edu.tw", "140.119.229.20").replace("https://", "http://"), timeout=10)
-        res.raise_for_status()
-        sleep(0.2)
-        soup = BeautifulSoup(res.content.decode("big5").encode("utf-8"), "html.parser")
-        rows = soup.find_all("tr")
-        for row in [x.find_all("td") for x in soup.find_all("tr") if x.find_all("td")[1].find("a")]:
-          teacherName = str(row[0].text)
-          teacherId = row[-1].find("a")["href"].split("statisticAll.jsp-tnum=")[1].split(".htm")[0]
-          teacherIdDict[teacherName] = teacherId
-    except Exception as e:
-      logging.error(e)
-      continue
-      
-  # Write teacherIdDict back to file
-  with open(os.path.join(dirPath, "_data", "teachers.json"), "w+") as f:
-    f.write(json.dumps(teacherIdDict))
-    f.close()
-  
-  # Delete courses from track list
-  tqdmCourses =  tqdm.tqdm(courses, leave=False)
-  for course in tqdmCourses:
-    try:
-      sleep(0.2)
-      courseId = str(course["subNum"])
-      tqdmCourses.set_postfix_str("Deleting {}".format(courseId))
-      user.deleteTrack(courseId)
-    except Exception as e:
-      logging.error(e)
-      continue
-  
-  print("Fetch TeacherId done at {}".format(datetime.datetime.now()))
+    
+    print("Fetch TeacherId done at {}".format(datetime.datetime.now()))
+  else:
+    print("Skipping Fetch TeacherId")
   
   # ==============================
   # \ 3. Fetch Rates and Details \
   # ==============================
   
-  # Read teacher list
-  with open(os.path.join(dirPath, "_data", "teachers.json"), "r") as f:
-    newTeacherList = json.loads(f.read())
-  with open(os.path.join(dirPath, "old_data", "1111_teachers.json"), "r") as f:
-    oldTeacherList = json.loads(f.read())
-  teacherList = {**newTeacherList,**oldTeacherList}
-  
-  # Run through all teacherId, and fetch courses of teachers
-  teachers = tqdm.tqdm(teacherList, total=len(teacherList), leave=False)
-  for teacher in teachers:
-    try:
-      teacherId = teacherList[teacher]
-      teachers.set_postfix_str("processing: {} {}".format(teacherId, teacher))
-      location = "http://newdoc.nccu.edu.tw/teaschm/1112/statistic.jsp-tnum={}.htm".format(teacherId)
-      res = requests.get(location)
-      res.raise_for_status()
-      soup = BeautifulSoup(res.content.decode("big5").encode("utf-8"), "html.parser")
-      courses = soup.find_all('table')[2].find_all('tr')
-      availableCourses = [x.find_all('td') for x in courses if x.find_all('td')[-1].find("a")]
-      tqdmCourses = tqdm.tqdm(availableCourses, total=len(availableCourses), leave=False)
-      
-      for row in tqdmCourses:
-        courseId = "{}{}{}".format(row[0].text, row[1].text, row[2].text)
-        tqdmCourses.set_postfix_str("processing: {}".format(courseId))
-        detail = fetchDescription(courseId)
-        rates = fetchRate("http://newdoc.nccu.edu.tw/teaschm/1112/" + row[-1].find("a")["href"])
-        logging.debug(detail)
-        logging.debug(rates)
-          
-    except Exception as e:
-      logging.error(e)
-      continue
-  
-  print("Fetch Rates and Details done at {}".format(datetime.datetime.now()))
+  if not programOptions["skip_rate"]:
+    # Read teacher list
+    with open(os.path.join(dirPath, "_data", "teachers.json"), "r") as f:
+      newTeacherList = json.loads(f.read())
+    with open(os.path.join(dirPath, "old_data", "1111_teachers.json"), "r") as f:
+      oldTeacherList = json.loads(f.read())
+    teacherList = {**newTeacherList,**oldTeacherList}
+    
+    # Run through all teacherId, and fetch courses of teachers
+    teachers = tqdm.tqdm(teacherList, total=len(teacherList), leave=False)
+    for teacher in teachers:
+      try:
+        teacherId = teacherList[teacher]
+        teachers.set_postfix_str("processing: {} {}".format(teacherId, teacher))
+        location = "http://newdoc.nccu.edu.tw/teaschm/1112/statistic.jsp-tnum={}.htm".format(teacherId)
+        res = requests.get(location)
+        res.raise_for_status()
+        soup = BeautifulSoup(res.content.decode("big5").encode("utf-8"), "html.parser")
+        courses = soup.find_all('table')[2].find_all('tr')
+        availableCourses = [x.find_all('td') for x in courses if x.find_all('td')[-1].find("a")]
+        tqdmCourses = tqdm.tqdm(availableCourses, total=len(availableCourses), leave=False)
+        
+        for row in tqdmCourses:
+          courseId = "{}{}{}".format(row[0].text, row[1].text, row[2].text)
+          tqdmCourses.set_postfix_str("processing: {}".format(courseId))
+          detail = fetchDescription(courseId)
+          rates = fetchRate("http://newdoc.nccu.edu.tw/teaschm/1112/" + row[-1].find("a")["href"])
+          logging.debug(detail)
+          logging.debug(rates)
+
+          # Create folder if not exist
+          path = os.path.join(dirPath, "result", teacher, detail["qrysub"]["subNam"])
+          if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+            
+          # Write detail back to file
+          with open(os.path.join(path, "{}.json".format(courseId)), "w+") as f:
+            f.write(json.dumps({"comments": rates, "details": detail}))
+            f.close()
+            
+      except Exception as e:
+        logging.error(e)
+        continue
+    
+    print("Fetch Rates and Details done at {}".format(datetime.datetime.now()))
+  else:
+    print("Skipping Fetch Rate")
   
 # translateRate()
